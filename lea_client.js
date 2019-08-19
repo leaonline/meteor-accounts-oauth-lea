e5ab60b6e1b0247169ba679c1a7c027389d31a31
Jan Kuester <jkuester@uni-bremen.de>
2019-08-19 16:54:35 +0200

    reduced complexity and removed unneccessary features

--- lea_client.js
+++ lea_client.js
@@ -1,4 +1,8 @@
-Lea = {};
+/* global ServiceConfiguration, Lea */
+import { Random } from 'meteor/random'
+import { OAuth } from 'meteor/oauth'
+
+Lea = Lea || {}
 
 // Request Lea credentials for the user
 //
@@ -7,47 +11,47 @@ Lea = {};
 //   completion. Takes one argument, credentialToken on success, or Error on
 //   error.
 Lea.requestCredential = (options, credentialRequestCompleteCallback) => {
-
   // support both (options, callback) and (callback).
   if (!credentialRequestCompleteCallback && typeof options === 'function') {
-    credentialRequestCompleteCallback = options;
-    options = {};
+    credentialRequestCompleteCallback = options
+    options = {}
   }
 
-  const config = ServiceConfiguration.configurations.findOne({service: 'lea'});
-  if (!config || !config.dialogUrl) {
-    credentialRequestCompleteCallback && credentialRequestCompleteCallback(
-      new ServiceConfiguration.ConfigError());
-    return;
+  const serviceConfig = ServiceConfiguration.configurations.findOne({ service: 'lea' })
+  if (!serviceConfig || !serviceConfig.dialogUrl) {
+    credentialRequestCompleteCallback && credentialRequestCompleteCallback(new ServiceConfiguration.ConfigError())
+    return
   }
 
-  const credentialToken = Random.secret();
-  const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent);
-  const display = mobile ? 'touch' : 'popup';
+  const credentialToken = Random.secret()
+  const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|Windows Phone/i.test(navigator.userAgent)
+  const display = mobile ? 'touch' : 'popup'
 
-  let scope = "email";
-  if (options && options.requestPermissions)
-    scope = options.requestPermissions.join(',');
-
-  const loginStyle = OAuth._loginStyle('lea', config, options);
-
-  let loginUrl =
-    `${config.dialogUrl}?client_id=${encodeURIComponent(config.appId)}` +
-    `&redirect_uri=${encodeURIComponent(config.redirectUri)}` +
-    `&display=${encodeURIComponent(display)}&scope=${encodeURIComponent(scope)}` +
-    `&response_type=code` +
-    `&state=${OAuth._stateParam(loginStyle, credentialToken, options && options.redirectUrl)}`;
-
-  // Handle authentication type (e.g. for force login you need auth_type: "reauthenticate")
-  if (options && options.auth_type) {
-    loginUrl += `&auth_type=${encodeURIComponent(options.auth_type)}`;
+  let scope = [ 'email' ]
+  if (options && options.requestPermissions) {
+    scope = scope.concat(options.requestPermissions)
   }
+  const flatScope = scope.map(encodeURIComponent).join('+')
+  const loginStyle = OAuth._loginStyle('lea', serviceConfig, options)
+  const dialogUrl = serviceConfig.dialogUrl
+  const redirectUrl = OAuth._redirectUri('lea', serviceConfig)
+  const state = OAuth._stateParam(loginStyle, credentialToken, options && options.redirectUrl)
+
+  const loginUrl =
+    `${dialogUrl}` +
+    `?response_type=code` +
+    `&client_id=${serviceConfig.clientId}` +
+    `&scope=${flatScope}` +
+    `&display=${display}` +
+    `&redirect_uri=${encodeURIComponent(redirectUrl)}` +
+    `&state=${encodeURIComponent(state)}`
 
   OAuth.launchLogin({
-    loginService: "lea",
+    loginService: 'lea',
     loginStyle,
     loginUrl,
     credentialRequestCompleteCallback,
     credentialToken,
-  });
-};
+    popupOptions: { width: 900, height: 450 }
+  })
+}
--- lea_server.js
+++ lea_server.js
@@ -1,114 +1,89 @@
-Lea = {};
-import crypto from 'crypto';
+/* global Lea, ServiceConfiguration */
+import { Meteor } from 'meteor/meteor'
+import { OAuth } from 'meteor/oauth'
+import { HTTP } from 'meteor/http'
+import { WebApp } from 'meteor/webapp'
+
+Lea = Lea || {}
 
 const settings = Meteor.settings.oauth.lea
-const auth_access_token_url = settings.accessTokenUrl
-const auth_identity_url = settings.identityUrl
 
-Lea.handleAuthFromAccessToken = (accessToken, expiresAt) => {
-  // include basic fields from lea
-  const whitelisted = ['id', 'email', 'name', 'first_name', 'last_name',
-    'middle_name', 'name_format', 'picture', 'short_name'];
-
-  const identity = getIdentity(accessToken, whitelisted);
-
-  const fields = {};
-  whitelisted.forEach(field => fields[field] = identity[field]);
-  const serviceData = {
-    accessToken,
-    expiresAt,
-    ...fields,
-  };
-
-  return {
-    serviceData,
-    options: {profile: {name: identity.name}}
-  };
-};
+let userAgent = 'Meteor'
+if (Meteor.release)
+  userAgent += `/${Meteor.release}`
 
 OAuth.registerService('lea', 2, null, query => {
-  const response = getTokenResponse(query);
-  const { accessToken } = response;
-  const { expiresIn } = response;
+  console.log('lea service', query)
+  const accessToken = getAccessToken(query)
+  const identity = getIdentity(accessToken)
 
-  return Lea.handleAuthFromAccessToken(accessToken, (+new Date) + (1000 * expiresIn));
-});
-
-// checks whether a string parses as JSON
-const isJSON = str => {
-  try {
-    JSON.parse(str);
-    return true;
-  } catch (e) {
-    return false;
-  }
-};
-
-// returns an object containing:
-// - accessToken
-// - expiresIn: lifetime of token in seconds
-const getTokenResponse = query => {
-  const config = ServiceConfiguration.configurations.findOne({service: 'lea'});
-  if (!config)
-    throw new ServiceConfiguration.ConfigError();
-
-  let responseContent;
-  try {
-    // Request an access token
-    responseContent = HTTP.get(
-      auth_access_token_url, {
-        params: {
-          client_id: config.appId,
-          redirect_uri: OAuth._redirectUri('lea', config),
-          client_secret: OAuth.openSecret(config.secret),
-          code: query.code
-        }
-      }).data;
-  } catch (err) {
-    throw Object.assign(
-      new Error(`Failed to complete OAuth handshake with Lea. ${err.message}`),
-      { response: err.response },
-    );
-  }
-
-  const accessToken = responseContent.access_token;
-  const tokenExpires = responseContent.expires_in;
-
-  if (!accessToken) {
-    throw new Error("Failed to complete OAuth handshake with lea " +
-      `-- can't find access token in HTTP response. ${responseContent}`);
-  }
   return {
-    accessToken: accessToken,
-    expiresIn: tokenExpires
-  };
-};
+    serviceData: {
+      id: identity.id,
+      accessToken: OAuth.sealSecret(accessToken),
+      email: identity.email || '',
+      username: identity.login
+    },
+    options: { profile: { name: identity.name } }
+  }
+})
 
-const getIdentity = (accessToken, fields) => {
-  const config = ServiceConfiguration.configurations.findOne({service: 'lea'});
-  if (!config)
-    throw new ServiceConfiguration.ConfigError();
+WebApp.rawConnectHandlers.use('_oauth', function (req, res, next) {
+  console.log('_oauth/lea request')
+  console.log(req.headers)
+})
 
-  // Generate app secret proof that is a sha256 hash of the app access token, with the app secret as the key
-  const hmac = crypto.createHmac('sha256', OAuth.openSecret(config.secret));
-  hmac.update(accessToken);
+const getAccessToken = query => {
+  const config = ServiceConfiguration.configurations.findOne({ service: 'lea' })
+  if (!config) {
+    throw new ServiceConfiguration.ConfigError()
+  }
 
+  console.log(config)
+
+  let response
   try {
-    return HTTP.get(auth_identity_url, {
-      params: {
-        access_token: accessToken,
-        appsecret_proof: hmac.digest('hex'),
-        fields: fields.join(",")
-      }
-    }).data;
+    response = HTTP.post(
+      settings.accessTokenUrl, {
+        headers: {
+          Accept: 'application/json',
+          'User-Agent': userAgent
+        },
+        params: {
+          code: query.code,
+          client_id: config.clientId,
+          client_secret: OAuth.openSecret(config.secret),
+          redirect_uri: OAuth._redirectUri('lea', config),
+          state: query.state
+        }
+      })
   } catch (err) {
     throw Object.assign(
-      new Error(`Failed to fetch identity from Lea. ${err.message}`),
-      { response: err.response },
-    );
+      new Error(`Failed to complete OAuth handshake with lea. ${err.message}`),
+      { response: err.response }
+    )
   }
-};
+  // if the http response was a json object with an error attribute
+  if (response.data.error) {
+    throw new Error(`Failed to complete OAuth handshake with lea. ${response.data.error}`)
+  } else {
+    return response.data.access_token
+  }
+}
 
-Lea.retrieveCredential = (credentialToken, credentialSecret) =>
-  OAuth.retrieveCredential(credentialToken, credentialSecret);
+const getIdentity = accessToken => {
+  try {
+    return HTTP.get(
+      settings.identityUrl, {
+        headers: { 'User-Agent': userAgent },
+        params: { access_token: accessToken }
+      }).data
+  } catch (err) {
+    throw Object.assign(
+      new Error(`Failed to fetch identity from lea. ${err.message}`),
+      { response: err.response }
+    )
+  }
+}
 
+Lea.retrieveCredential = (credentialToken, credentialSecret) => OAuth.retrieveCredential(credentialToken, credentialSecret)
