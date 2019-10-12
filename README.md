9bea6cec7b202686681c8077f674ed0295d87d09
jankapunkt <jkuester@uni-bremen.de>
2019-10-12 14:26:34 +0200

    use ServiceConfig to retrieve identity url

---
+++ README.md
@@ -0,0 +1,3 @@
+# Meteor OAuth lea
+
+OAuth service configuration and handling for logging in with lea (your custom OAuth server).
--- package.js
+++ package.js
@@ -1,21 +1,21 @@
 Package.describe({
   name: 'leaonline:oauth-lea',
-  version: '0.0.1',
+  version: '1.0.0',
   // Brief, one-line summary of the package.
-  summary: '',
+  summary: 'OAuth package to provide authorizaiton code login with lea',
   // URL to the Git repository containing the source code for this package.
-  git: '',
+  git: 'git@github.com:leaonline/meteor-accounts-oauth-lea.git',
   // By default, Meteor will default to using README.md for documentation.
   // To avoid submitting documentation, set this field to null.
   documentation: 'README.md'
 });
 
 Package.onUse(api => {
-  api.use('ecmascript', ['client', 'server']);
-  api.use('oauth2', ['client', 'server']);
-  api.use('oauth', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('random', 'client');
+  api.use('ecmascript@0.12.7', ['client', 'server']);
+  api.use('oauth2@latest', ['client', 'server']);
+  api.use('oauth@1.2.8', ['client', 'server']);
+  api.use('http@latest', ['server']);
+  api.use('random@latest', 'client');
   api.use('service-configuration', ['client', 'server']);
 
   api.addFiles('lea_client.js', 'client');
