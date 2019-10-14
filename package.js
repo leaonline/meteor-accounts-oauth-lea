358d7db1ee55400a117f86908d65c0fd4c40da10
Jan Küster <jkuester@uni-bremen.de>
2019-10-14 21:45:41 +0200

    publish 1.0.0

--- package.js
+++ package.js
@@ -12,11 +12,11 @@ Package.describe({
 
 Package.onUse(api => {
   api.use('ecmascript@0.12.7', ['client', 'server']);
-  api.use('oauth2', ['client', 'server']);
+  api.use('oauth2@1.2.1', ['client', 'server']);
   api.use('oauth@1.2.8', ['client', 'server']);
-  api.use('http', ['server']);
-  api.use('random', 'client');
-  api.use('service-configuration', ['client', 'server']);
+  api.use('http@1.4.2', ['server']);
+  api.use('random@1.1.0', 'client');
+  api.use('service-configuration@1.0.11', ['client', 'server']);
 
   api.addFiles('lea_client.js', 'client');
   api.addFiles('lea_server.js', 'server');
