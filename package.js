1598b4ae346cffc1497e3f5c2c2ed2df7972dc69
Jan Kuester <jkuester@uni-bremen.de>
2019-11-15 10:48:31 +0100

    published 1.0.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* eslint-env meteor */
 Package.describe({
   name: 'leaonline:oauth-lea',
-  version: '1.0.0',
+  version: '1.0.1',
   // Brief, one-line summary of the package.
   summary: 'OAuth package to provide authorizaiton code login with lea',
   // URL to the Git repository containing the source code for this package.
